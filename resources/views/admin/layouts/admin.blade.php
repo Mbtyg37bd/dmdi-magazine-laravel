f619348008c739aaebd44885232ae60a1bce0eab
fitra <f.itrakeren23@gmail.com>
2026-01-02 01:25:42 +0700

    perbaikan yang telah di selesai di bagian admin belum lagi ke frontend

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -1,4 +1,4 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons. css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     
     <!-- BLOCK TINYMCE -->
     <script>
@@ -86,7 +86,7 @@
                             </a>
                         </li>
 
-                        
+
 
                         @auth
                             @if(auth()->user()->is_admin)
